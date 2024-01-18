5a2ba5ddb83553e937a607707dce941c786da9eb
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2024-01-18 04:42:56 +0300

    добавление hashmap для учета результатов игр (по условию)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,23 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        double winsCounter = 0;
         int gamesQuantity = 1000;
+        HashMap<Integer, Boolean> games = new HashMap<>();
         for (int i = 0; i < gamesQuantity; i++) {
             LetsMakeDealGame game = new LetsMakeDealGame(3);
-            if (game.play()) winsCounter++;
+            games.put(i, game.play());
         }
-        double winPercent = winsCounter/gamesQuantity*100;
-        System.out.printf("General win percent is: %.2f%%\n", winPercent);
+        printResults(games, gamesQuantity);
+    }
+
+    private static void printResults(HashMap<Integer, Boolean> games, int gamesQuantity){
+        int wins = (int) games.values().stream().filter(value -> value).count();
+        int losses = gamesQuantity - wins;
+        double winPercent = (double) wins/gamesQuantity*100;
+        System.out.printf("Total games: %d, wins: %d, losses: %d, general win percent is: %.2f%%\n",
+                gamesQuantity, wins, losses, winPercent);
     }
 }
